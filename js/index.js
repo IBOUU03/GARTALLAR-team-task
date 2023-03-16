5ab04e7e102bf27ea06858f28d27b385eec15049
Samedusa4real <samadnm@code.edu.az>
2023-03-16 06:50:49 -0800

    armenian site created pls nobody cna reach

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-let indicator = document.querySelector(".indicator").children;
+// let indicator = document.querySelector(".indicator").children;
+let ul = document.querySelector(".items")
+let indicator = document.querySelectorAll(".indicator>li");
 
 let items = document.querySelectorAll(".items .item");
 
@@ -11,119 +13,91 @@ items.forEach((item) => {
     }
 })
 
-for(let i=0; i<indicator.length; i++){
-    indicator[i].addEventListener("click", function(){
-        for(let x=0; x<indicator.length; x++){
-            indicator[x].classList.remove("active");
-        }
+indicator.forEach((el) => {
+    el.addEventListener("click", function() {
+        indicator.forEach((el) => {
+            el.classList.remove("active")
+        })
         this.classList.add("active")
         const displayItems = this.getAttribute("data-filter");
 
-        for (let z = 0; z < items.length; z++) {
-            items[z].style.transform = "scale(0)";
+        items.forEach((el) => {
+            el.style.transform = "scale(0)";
             setTimeout(() => {
-                items[z].style.display = "none";
+                el.style.display = "none";
             }, 500)
             
-            if(items[z].getAttribute("data-category") == displayItems || displayItems == "all"){
-                items[z].style.transform = "scale(1)";
-            setTimeout(() => {
-                items[z].style.display = "block";
-            }, 500)
+            if(el.getAttribute("data-category") == displayItems || displayItems == "all"){
+                el.style.transform = "scale(1)";
+                setTimeout(() => {
+                    el.style.display = "block";
+                }, 500)
             }
-        }
-    }
-    )
+        })
+    })
+})
+
+const costEl = document.querySelectorAll(".cost-text")
+const selectbox = document.querySelector("#select")
+console.log(items);
+console.log(items[0].childNodes[5].textContent)
+console.log(ul.children)
+selectbox.addEventListener("change", (e) => {
+  if(selectbox.value === "LowToHigh") {
+    Array.prototype.slice.call(ul.children)
+            .map(function (x) { 
+                console.log(x.classList)
+                return ul.removeChild(x); 
+            })
+            .filter((el) => el.classList.length != 2)
+            .sort(function (x, y) { 
+                
+                console.count(x.childNodes[5].textContent.split("$")[1])
+                console.count(y.childNodes[5].textContent.split("$")[1]);
+                return +x.childNodes[5].textContent.split("$")[1] -  +y.childNodes[5].textContent.split("$")[1]
+               
+            })
+            .forEach(function (x) { ul.appendChild(x); });
+  }
+  if (selectbox.value === "HighToLow") {
+    Array.prototype.slice.call(ul.children)
+            .map(function (x) { 
+                console.log(x.classList)
+                return ul.removeChild(x); 
+            })
+            .filter((el) => el.classList.length != 2)
+            .sort(function (x, y) { 
+                
+                console.count(x.childNodes[5].textContent.split("$")[1])
+                console.count(y.childNodes[5].textContent.split("$")[1]);
+                return +y.childNodes[5].textContent.split("$")[1] -  +x.childNodes[5].textContent.split("$")[1]
+                
+            })
+            .forEach(function (x) { ul.appendChild(x); });
+  } 
+})
+
+
+
+// 
+
+// let items = document.querySelectorAll(".items .item");
+
+deleteItem()
+
+function deleteItem(){
+    const deleteBtn = document.querySelectorAll(".delete-btn")
+    deleteBtn.forEach(function(btn){
+        btn.addEventListener("click",function(){
+            const item =btn.parentNode ;
+            const grandparent = item.parentNode
+            const grandgrandparent = grandparent.parentNode
+            grandgrandparent.classList.add("d-none")
+        })
+
+    })
+
 }
 
 
-// (function() {
-		
-//     let field = document.querySelector('.items');
-//     let li = Array.from(field.children);
 
-//     function FilterProduct() {
-//         for(let i of li){
-//             const name = i.querySelector('strong');
-//             const x = name.innerText;
-//             i.setAttribute("data-category", x);
-//         }
-
-//         let indicator = document.querySelector('.indicator').children;
-
-//         this.run = function() {
-//             for(let i=0; i<indicator.length; i++)
-//             {
-//                 indicator[i].onclick = function () {
-//                     for(let x=0; x<indicator.length; x++)
-//                     {
-//                         indicator[x].classList.remove('active');
-//                     }
-//                     this.classList.add('active');
-//                     const displayItems = this.getAttribute('data-filter');
-
-//                     for(let z=0; z<li.length; z++)
-//                     {
-//                         li[z].style.transform = "scale(0)";
-//                         setTimeout(()=>{
-//                             li[z].style.display = "none";
-//                         }, 500);
-
-//                         if ((li[z].getAttribute('data-category') == displayItems) || displayItems == "all")
-//                          {
-//                              li[z].style.transform = "scale(1)";
-//                              setTimeout(()=>{
-//                                 li[z].style.display = "block";
-//                             }, 500);
-//                          }
-//                     }
-//                 };
-//             }
-//         }
-//     }
-
-//     function SortProduct() {
-//         let select = document.getElementById('select');
-//         let ar = [];
-//         for(let i of li){
-//             const last = i.lastElementChild;
-//             const x = last.textContent.trim();
-//             const y = Number(x.substring(1));
-//             i.setAttribute("data-price", y);
-//             ar.push(i);
-//         }
-//         this.run = ()=>{
-//             addevent();
-//         }
-//         function addevent(){
-//             select.onchange = sortingValue;
-//         }
-//         function sortingValue(){
-        
-//             if (this.value === 'Default') {
-//                 while (field.firstChild) {field.removeChild(field.firstChild);}
-//                 field.append(...ar);	
-//             }
-//             if (this.value === 'LowToHigh') {
-//                 SortElem(field, li, true)
-//             }
-//             if (this.value === 'HighToLow') {
-//                 SortElem(field, li, false)
-//             }
-//         }
-//         function SortElem(field,li, asc){
-//             let  dm, sortli;
-//             dm = asc ? 1 : -1;
-//             sortli = li.sort((a, b)=>{
-//                 const ax = a.getAttribute('data-price');
-//                 const bx = b.getAttribute('data-price');
-//                 return ax > bx ? (1*dm) : (-1*dm);
-//             });
-//              while (field.firstChild) {field.removeChild(field.firstChild);}
-//              field.append(...sortli);	
-//         }
-//     }
-
-//     new FilterProduct().run();
-//     new SortProduct().run();
-// })();
